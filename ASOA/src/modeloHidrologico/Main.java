dc99bee1faf9f0d4bed01576c9f62bcb9c191dee
Orne <Orne@EXO>
2018-11-14 21:26:02 -0300

    Tabla de corridas

--- ASOA/src/modeloHidrologico/Main.java
+++ ASOA/src/modeloHidrologico/Main.java
@@ -1,48 +1,43 @@
 package modeloHidrologico;
 
 import java.util.*;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-	
-	public static void main (String[]args) {
+		public static void main (String[]args) {
+			
 		Scanner print = new Scanner (System.in);
 		
-		System.out.println("_________________________________________________________________________________________");
-		System.out.println("'-----------------  INGRESO DE LOS VALORES PARA LLEVAR A CABO LA SIMULACION:-------------'");
-		
-		System.out.println(" " );
-		System.out.println(">>Ingrese el numero de iteraciones de la simulacion: ");
+		System.out.println(" ");
+		System.out.println("");
+		System.out.println("-----------------------INGRESO DE LOS VALORES PARA LLEVAR A CABO LA SIMULACION----------------------");
+		System.out.print(">>Ingrese el numero de iteraciones de la simulacion: ");
 		Integer corridas = print.nextInt();
-
-		System.out.println(">> Cantidad de dias que se simularan (EXT): ");
+		System.out.print(">> Cantidad de dias que se simularan (EXT): ");
 		Integer EXT = print.nextInt();
 		
 		
-		System.out.println("_________________________________________________________________________________________");
 		/* INICIO DE LA APLICACION DEL METODO MULTIPLICATIVO DE LAS CONGRUENCIAS (ejecucionMetMultiCongru())**/
-		System.out.println("-----------------  INGRESO DE LOS VALORES PARA EL METODO MULTIPLICATIVO DE LAS CONGRUENCIAS:-------------");
-		System.out.println("INGRESE LOS SIGUIENTES VALORES ENTEROS Y MAYORES A 0        ");
+		System.out.println("");
+		System.out.println("-----------------INGRESO DE LOS VALORES PARA EL METODO MULTIPLICATIVO DE LAS CONGRUENCIAS-------------");
+		System.out.println("");
+		System.out.println("INGRESE LOS SIGUIENTES VALORES ENTEROS Y MAYORES A 0 ");
 		
-		System.out.println(">> Semilla: ");
+		System.out.print(">> Semilla: ");
 		Integer semilla = print.nextInt();
-		System.out.println(">>Modulo (debe ser la unidad (1) seguida de ceros(0) y mayor a la semilla):  ");
+		System.out.print(">>Modulo (debe ser la unidad (1) seguida de ceros(0) y mayor a la semilla): ");
 		Integer modulo = print.nextInt();
 		
-
-		System.out.println("Para calcular el parametro a necesitaremmos los siguientes valores :       \n");
-		System.out.println(">>Parámetro t (un valor entero cualquiera):  ");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Para calcular el parametro a necesitaremmos los siguientes valores :  \n");
+		System.out.print(">>Parametro t (un valor entero cualquiera):  ");
 		Integer t = print.nextInt();
 		
-		System.out.println("Para el parametro p elija uno de los siguientes números ");
+		System.out.println("Para el parametro p elija uno de los siguientes numeros ");
 		System.out.println("3, 11, 13, 19, 21, 27, 29, 37, 53, 59, 61, 67, 69, 77, 83, 91");
-		System.out.println(">>Parámetro \"p\": ");
+		System.out.print(">>Parametro \"p\": ");
 		Integer p = print.nextInt();
 		
 
@@ -60,15 +55,21 @@ public class Main {
      		 * %DIRECTAMENTE LLAMAREMOS A LA FUNCION Y NO IMPRIMIREMOS NINGUN VALOR
 			 * */
 			/* INICIO BLOQUE DE CODIGO EjecucionChi()*/
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
 			System.out.println("-------------------------------------------------------------------------------------");
 			System.out.println("             PRUEBA CHI CUADRADO PARA VERIFICAR QUE UNA SERIE DE NUMEROS             ");
 			System.out.println("                    PSEUDOALEATORIOS SIGA UNA DISTRIBUCION UNIFORME                  ");
 			System.out.println("-------------------------------------------------------------------------------------");
 			
-			System.out.print(">>Ingrese el número de subintervalos considerados: ");
+			System.out.println("");
+			System.out.println("");
+			System.out.print(">>Ingrese el numero de subintervalos considerados: ");
 			Integer ni = print.nextInt();
-			System.out.print(">>Ingrese nivel de significación considerado entre [0,1]: (sugerido 0,05)");
+			System.out.print(">>Ingrese nivel de significacion considerado entre [0,1]: (sugerido 0,05)");
 			Double alpha = print.nextDouble();
+			
 			//Chi2(ArrayList<Double> serie, int ni, double alpha )
 			Chi2 mch2 = new Chi2(serie, ni, alpha);
 			
@@ -85,10 +86,11 @@ public class Main {
 			//CHI2T
 			double chi2t = mch2.getChi2Teorico();
 			
-			System.out.println("-------------------------------------------------------------------------------------");
-			System.out.println("--------------------------- TABLA DE RESULTADOS PARCIALES ---------------------------");
-			System.out.println("-------------------------------------------------------------------------------------");
-
+			System.out.println("");
+			System.out.println("_____________________________________________________________________________________");
+			System.out.println("--------------------------TABLA DE RESULTADOS PARCIALES------------------------------");
+			System.out.println("_____________________________________________________________________________________");
+			System.out.println("");
 			System.out.printf("%2sClases%8sIntervalo%8sF. Observadas%2sF. Esperada%2s((fo-fe)^2)/fe","","","","","");
 			System.out.println("");
 			for (int i= 0;i<ni; i++) {
@@ -96,14 +98,15 @@ public class Main {
 				System.out.println("");
 			}
 			System.out.print("");
-			System.out.println("-------------------------------------------------------------------------------------");
+			//System.out.println("-------------------------------------------------------------------------------------");
 			System.out.println("");
 
 			
-
-			System.out.println("-------------------------------------------------------------------------------------");
+			System.out.println("");
+			System.out.println("______________________________________________________________________________________");
 			System.out.println("----------------------------------- RESULTADOS --------------------------------------");
-			System.out.println("-------------------------------------------------------------------------------------");
+			System.out.println("______________________________________________________________________________________");
+			System.out.println("");
 			System.out.printf("%2s* Grados de libertad = %d", "", ni-1);
 			System.out.println("");
 			if(resultado) {
@@ -132,16 +135,17 @@ public class Main {
 				/**
 				 * inicio de las corridas
 				 * */
-				System.out.println("-------------------------------------------------------------------------------------");
+				System.out.println("");
+				System.out.println("_____________________________________________________________________________________");
 				System.out.println("------------------------- INICIAREMOS EL MODELO HIDROLOGICO -------------------------");
-				System.out.println("-------------------------------------------------------------------------------------");
+				System.out.println("_____________________________________________________________________________________");
 				
 				System.out.println("");
-				System.out.println(" >>Ingrese el valor del caudal inicial (QIN):");
+				System.out.print(" >>Ingrese el valor del caudal inicial (QIN):");
 				int QIN = print.nextInt();
-				System.out.println(" >>Ingrese el valor del caudal minimo alcanzado (CIN):");
+				System.out.print(" >>Ingrese el valor del caudal minimo alcanzado (CIN):");
 				int CIN = print.nextInt();
-				System.out.println(" >>Ingrese el valor del caudal maximo alcanzado (CSU)):");
+				System.out.print(" >>Ingrese el valor del caudal maximo alcanzado (CSU)):");
 				int CSU = print.nextInt();
 				
 				//vector de los caudales medios obtenidos en cada una de las corridas
@@ -160,8 +164,9 @@ public class Main {
 				ArrayList<Integer> subMuestra = new ArrayList<Integer>();
 				int inicio = 0;
 				int fin = EXT;
+				
 				while ( i < corridas) {
-					//ASIGNAMOS UNA PORCION DE LA MUESTRA POR CADA CORRIDA DEL TAMAÑO DE EXT
+					//ASIGNAMOS UNA PORCION DE LA MUESTRA POR CADA CORRIDA DEL TAMAñO DE EXT
 					for (int sub = inicio; sub <= fin; sub++) {
 						subMuestra.add(muestraAleatoria.get(sub));
 					}
@@ -169,8 +174,9 @@ public class Main {
 					//[VQIN, QDI, vTOTAL_ACUM, vTSQ, vTIQ] =MetodoHidrico(MADel,QIN,CIN,CSU);
 					MetodoHidrico metHid = new MetodoHidrico(subMuestra);
 					metHid.aplicarMetodo(QIN, CIN, CSU);
-					
-					System.out.print("-------------------------------------------------------------------------------------");
+					System.out.println("");
+					System.out.print("_____________________________________________________________________________________");
+					System.out.println("");
 					System.out.printf("Corrida N° %d", i);
 					System.out.println("");
 					System.out.printf("  >> Caudal Maximo obtenido: %d", metHid.getMaximo());
@@ -181,34 +187,33 @@ public class Main {
 					System.out.println("");
 					System.out.printf("  >> TIEMPO EN EL QUE EL CAUDAL FUE INFERIOR A %d, %d", CIN, metHid.getTiempoCaudalInferior());
 					System.out.println("");
-					System.out.printf("EL CAUDAL MEDIO DURANTE EL PERIDODO FUE DE: %.4f", (double)(metHid.getCaudalesAcumulados()/EXT));
+					System.out.printf(">>>>EL CAUDAL MEDIO DURANTE EL PERIDODO FUE DE: %.4f", (double)(metHid.getCaudalesAcumulados()/EXT));
 					System.out.println("");
 					System.out.print("-------------------------------------------------------------------------------------");
 					System.out.println("");
 					inicio = fin+1;
 					fin = fin + EXT;
-					i++;
-					/* Grafica  */
-									
-					DefaultCategoryDataset metHidro = new DefaultCategoryDataset();
-					
-					String c1="Maximo";
-					String c2="Minimo";
-					String c3="Acumulados";
-															
-					metHidro.setValue(metHid.getMinimo(),c1 ,"Caudal minimo");
-					metHidro.setValue(metHid.getMaximo(),c2,"Caudal Minimo");
-					metHidro.setValue(metHid.getCaudalesAcumulados(),c3,"Caudal Acumulado");
-					
-					JFreeChart f = ChartFactory.createBarChart("Hidrologia"," ","Caudal",metHidro,PlotOrientation.VERTICAL, true, false, false);
+					i++;}			
 				
-					ChartFrame frame= new ChartFrame("Hidrologia" ,f);
-					frame.pack();
-					frame.setVisible(true);
+				/*tabla de valores 
+				 * function [] = Imprimir(i,QSA,QIA,TSQ,TIQ,QMS)
+				 */
+				
+				System.out.println("");
+				System.out.println("_______________________________________________________________________________________________");
+				System.out.println("------------ TABLA DE DETALLA UN RESUMEN DE LOS VALORES OBTENIDOS EN CADA CORRIDA -------------");
+				System.out.println("_______________________________________________________________________________________________");
+				System.out.printf("%2sNro de Corridas%8sCaudal Maximo%8sCaudal Minimos%2sT.Superior%2sT.Inferior%2sCaudal Medio","","","","","","","");
+				System.out.println("");
+				for (int i1= 0;i1<corridas; i1++) {
+					System.out.printf("%5s%d %8s%.4f% 8s%.4f% 8s%.4f% 8s%.4f%","",i1,"",QSA.get(i1),"",QIA.get(i1),"",TSQ.get(i1),"",TIQ.get(i1),"",QMS.get(i1));
 					}
-					
+				System.out.print("");
 				
+				System.out.print("");
 				
+			
+							
 			}else {
 
 				System.out.printf("%2s** Chi2 Observado = %.3f > Chi2 Tabla = %.3f", "", chi2o,chi2t);
@@ -228,14 +233,10 @@ public class Main {
 				 * */
 			}
 			
-			
-			
-			
-			
-		}
-	
 		print.close();
-		
+	
 	}
-
 }
+}
+
+
